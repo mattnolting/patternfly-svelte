2b5ab157b829691e7bcbdd31c026a633e7b27e5f
Matt Nolting <matthew.nolting@gmail.com>
2024-12-12 11:24:33 -0500

    configuring build

--- svelte.config.js
+++ svelte.config.js
@@ -1,67 +1,45 @@
 // import adapter from '@sveltejs/adapter-auto';
 // import { sveltePreprocess } from 'svelte-preprocess'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
+import { sveltekit } from '@sveltejs/kit/vite';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-static';
+import { defineConfig } from 'vite'
+import path, { format } from 'path';
+// import { resolve } from 'path';
 
-import path from 'path';
-
-export const config = {
-	// mode: 'development',
+export default defineConfig({
+	mode: 'development',
 	extensions: ['.svelte' ],
 	type: [ 'module' ],
-	onwarn: (warning, handler) => handler(warning),
-	preprocess: [
-		vitePreprocess(),
-		// sveltePreprocess({
-		// 	scss: {
-		// 		outDir: './test',
-		// 		includePaths: ['src'], // Set the base directory for your SCSS files
-		// 		api: 'modern-compiler',
-		// 		outputStyle: 'expanded', // Optional: Can be 'compressed' or 'expanded'
-		// 	},
-		// })
+	// onwarn: (warning, handler) => handler(warning),
+  // preprocess: [
+	// 	sveltePreprocess({
+	//     // Preprocessor options, such as:
+	// 		// scss: {
+	// 		// 	prependData: `@import 'src/styles/variables.scss';`
+	// 		// },
+  //   	// typescript: true
+  // 	}),
+	// ],
+	plugins: [
+		sveltekit()
 	],
-  // plugin options
-  vitePlugin: {
-    exclude: [],
-    // experimental options
-    experimental: {}
-  },
-	compilerOptions: {
-		hydratable: true,
-	},
-	// kit: {
-	// 	adapter: adapter({
-	// 		// default options are shown. On some platforms
-	// 		// these options are set automatically — see below
-	// 		pages: 'build',
-	// 		assets: 'build',
-	// 		fallback: undefined,
-	// 		precompress: false,
-	// 		strict: true
-	// 	})
-	// }
+	preprocess: [sveltePreprocess()],
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		// adapter: adapter({
-		// 	// default options are shown. On some platforms
-		// 	// these options are set automatically — see below
-		// 	pages: 'build',
-		// 	assets: 'build',
-		// 	fallback: undefined,
-		// 	precompress: true,
-		// 	strict: false
-		// }),
-    adapter: adapter({
+		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-      out: 'build',
-      precompress: false,
-      envPrefix: 'tesss',
-			strick: false
-    }),
+			fallback: 'index.html',
+			trailingSlash: 'always'
+		}),
+		// files: {
+    //   hooks: 'src/hooks',
+    // },
+		// prerender: {
+		// 	default: true,
+		// },
 		alias: {
 			$images: path.resolve("./src/lib/images"),
 			$assets: path.resolve("./src/assets"),
@@ -73,6 +51,81 @@ export const config = {
 			$types: path.resolve("./src/types")
 		}
 	}
-};
+	// plugin options
+  // vitePlugin: {
+  //   exclude: [],
+  //   // experimental options
+  //   experimental: {},
+	// 	compilerOptions: {
+	// 		outDir: './dist'
+	// 	}
+  // },
+	// compilerOptions: {
+	// 	hydratable: true,
+	// 	outDir: './test'
+	// },
+	// kit: {
+	// 	adapter: adapter({
+	// 		// default options are shown. On some platforms
+	// 		// these options are set automatically — see below
+	// 		pages: 'build',
+	// 		assets: 'build',
+	// 		fallback: undefined,
+	// 		precompress: false,e
+	// 	})
+	// }
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+	// 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+	// 	adapter: adapter({
+	// 		// default options are shown. On some platforms
+	// 		// these options are set automatically — see below
+	// 		pages: 'build',
+	// 		// assets: 'build',
+	// 		fallback: undefined,
+	// 		precompress: true,
+	// 		strict: true,
+	// 	}),
+		// adapter: adapter({
+		// 	// default options are shown. On some platforms
+		// 	// these options are set automatically — see below
+		// 	// pages: 'build',
+		// 	// assets: 'build',
+		// 	// fallback: undefined,
+		// 	// precompress: false,
+		// 	// strict: true
+		// }),
+		// alias: {
+		// 	$images: path.resolve("./src/lib/images"),
+		// 	$assets: path.resolve("./src/assets"),
+		// 	$base: path.resolve("./src/base"),
+		// 	$components: path.resolve("./src/components"),
+		// 	$functions: path.resolve("./src/lib/functions"),
+		// 	$props: path.resolve("./src/lib"),
+		// 	$styles: path.resolve("./src/scss"),
+		// 	$types: path.resolve("./src/types")
+		// }
+	// },
+});
 
-export default config;
+// build: {
+// 	lib: {
+// 		entry: resolve(__dirname, 'lib/main.js'),
+// 		name: 'MyLib',
+// 		// the proper extensions will be added
+// 		fileName: 'my-lib',
+// 	},
+// 	rollupOptions: {
+// 		// make sure to externalize deps that shouldn't be bundled
+// 		// into your library
+// 		external: [],
+// 		output: {
+// 			// Provide global variables to use in the UMD build
+// 			// for externalized deps
+// 			globals: {
+// 				vue: 'Vue',
+// 			},
+// 		},
+// 	},
+// },
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
+import { resolve } from 'path'
+// import { rollup } from 'rollup';
+
+// import { fileURLToPath, URL } from 'node:url'
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  build: {
-		outDir: './build',
-	},
 	plugins: [
 		sveltekit()
+		// rollup()
 	],
+
 	resolve: {
 		alias: {
 			$images: path.resolve("./src/lib/images"),
@@ -17,7 +21,7 @@ export default defineConfig({
 			$components: path.resolve("./src/components"),
 			$functions: path.resolve("./src/lib/functions"),
 			$props: path.resolve("./src/lib"),
-			$styles: path.resolve("./src/scss"),
+			$styles: path.resolve("./src/*"),
 			$types: path.resolve("./src/types")
 		}
 	}
