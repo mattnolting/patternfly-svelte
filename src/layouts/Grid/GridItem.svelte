032ae7a34bcc51685f6435538354cc9259e520f0
Matt Nolting <matthew.nolting@gmail.com>
2025-01-15 22:59:06 -0500

    update

---
+++ src/layouts/Grid/GridItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import ComponentTemplate from '$lib/ComponentTemplate/ComponentTemplate.svelte';
+
+  let {
+    componentClass = 'pf-v6-l-grid__item',
+    cardTitle ='',
+    title = '',
+    text = 'sup',
+    children,
+    ...props
+  } = $props();
+</script>
+
+<ComponentTemplate {...props} componentTemplateClass={componentClass}>
+  {#if text}
+    {text}
+  {/if}
+
+  {@render children?.()}
+</ComponentTemplate>
